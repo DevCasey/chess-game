4ce0e13de5988b09e3c40e2575ec77f0ce9b3245
Angel Calkins <Angel.Calkins@hartehanks.com>
2019-08-12 11:13:15 -0500

    Working on the board.
    Might go back to the drawing board on this one...

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('working');
+const getMainId = document.getElementById('board');
+
+
+
+let createBoard = () => {
+    let table = document.createElement('table');
+    for(let i = 1; i < 9; i++) {
+        let tr = document.createElement('tr');
+        for(let j = 1; j< 0; j++) {
+            let td = document.createElement('td');
+            if(i%2 == j%2) {
+                td.className = "white";
+            } else {
+                td.className = "black";
+            }
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+}
+window.onload = createBoard;
